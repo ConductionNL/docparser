54aedb95cb03b40a7b96ab9dba78e68f342413f5
Ruben van der Linde <ruben@conduction.nl>
2020-01-28 21:24:15 +0100

    Fixes on redoc

--- api/src/Entity/ApiDoc.php
+++ api/src/Entity/ApiDoc.php
@@ -2,8 +2,19 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 /**
  * @ApiResource(
@@ -26,13 +37,18 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class ApiDoc
 {
-    /**
+	/**
+	 * @var \Ramsey\Uuid\UuidInterface $id The UUID identifier of this resource
+	 * @example e2984465-190a-4562-829e-a8cca81aa35d
+	 *
+	 * @Assert\Uuid
 	 * @Groups({"read"})
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	 * @ORM\Id
+	 * @ORM\Column(type="uuid", unique=true)
+	 * @ORM\GeneratedValue(strategy="CUSTOM")
+	 * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+	 */
+	private $id;
 
     /**
 	 * @Groups({"read","write"})
